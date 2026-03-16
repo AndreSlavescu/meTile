73fab4cc0859a8b9500e13d633ca6c74aa59f62e
github-action-benchmark <github@users.noreply.github.com>
2026-03-16 23:59:56 +0000

    add meTile Kernel Performance (customSmallerIsBetter) benchmark result for 55ceb4dfd8ca3bd22db016f9cd3a35ddd898abf2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773705015461,
+  "lastUpdate": 1773705596931,
   "repoUrl": "https://github.com/AndreSlavescu/meTile",
   "entries": {
     "meTile Kernel Performance": [
@@ -73,6 +73,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "fft_128x1024",
             "value": 424.44,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andre.slavescu@gmail.com",
+            "name": "AndreSlavescu",
+            "username": "AndreSlavescu"
+          },
+          "committer": {
+            "email": "andre.slavescu@gmail.com",
+            "name": "AndreSlavescu",
+            "username": "AndreSlavescu"
+          },
+          "distinct": true,
+          "id": "55ceb4dfd8ca3bd22db016f9cd3a35ddd898abf2",
+          "message": "contributing.md + performance dashboard",
+          "timestamp": "2026-03-16T19:59:09-04:00",
+          "tree_id": "083cff33e140156bc3eb32684120250964cee976",
+          "url": "https://github.com/AndreSlavescu/meTile/commit/55ceb4dfd8ca3bd22db016f9cd3a35ddd898abf2"
+        },
+        "date": 1773705595128,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "gemm_256x256x256",
+            "value": 381.13,
+            "unit": "us"
+          },
+          {
+            "name": "gemm_1024x1024x1024",
+            "value": 3458.13,
+            "unit": "us"
+          },
+          {
+            "name": "softmax_256x1024",
+            "value": 366.96,
+            "unit": "us"
+          },
+          {
+            "name": "softmax_1024x4096",
+            "value": 1110.79,
+            "unit": "us"
+          },
+          {
+            "name": "layernorm_256x1024",
+            "value": 466.15,
+            "unit": "us"
+          },
+          {
+            "name": "layernorm_1024x4096",
+            "value": 1084.98,
+            "unit": "us"
+          },
+          {
+            "name": "fft_1x256",
+            "value": 275.83,
+            "unit": "us"
+          },
+          {
+            "name": "fft_32x256",
+            "value": 239.88,
+            "unit": "us"
+          },
+          {
+            "name": "fft_1x1024",
+            "value": 251.98,
+            "unit": "us"
+          },
+          {
+            "name": "fft_128x1024",
+            "value": 351.33,
             "unit": "us"
           }
         ]
